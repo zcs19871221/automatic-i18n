158dfaae1d3ae5d07fbbd6ecd7d54511b3dbdb7c
chengsi <chengsi.zhang@se.com>
2024-01-08 13:14:58 +0800

    fff

--- scripts/run.ts
+++ scripts/run.ts
@@ -1,11 +1,27 @@
 #!/usr/bin/env node
+import * as path from 'path';
+import * as fs from 'fs';
 import { LocaleReplacer } from '../src';
 
-const inputFiles = process.argv.slice(2);
-console.log(inputFiles);
+const root = process.cwd();
+let dirsToReplace = [
+  'components',
+  'hooks',
+  'middleware',
+  'modules',
+  'pages',
+  'utils',
+];
+if (process.argv.slice(2).length > 0) {
+  dirsToReplace = process.argv.slice(2);
+}
 LocaleReplacer.replace({
   projectDir: process.cwd(),
   fileReplaceOverwirte: true,
-  filesOrDirsToReplace: inputFiles,
+  filesOrDirsToReplace: dirsToReplace
+    .map((f) => path.join(root, f))
+    .filter((f) => {
+      return fs.lstatSync(f).isDirectory();
+    }),
   importPath: 'I18',
 });
