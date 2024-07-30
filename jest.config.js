d95f92de568e1003b69ca9706e3457b0b42ae9f4
SESA731485 <cszhang@se.com>
2024-07-30 13:55:10 +0800

    tmp

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,7 @@ module.exports = {
     '^.+\\.tsx?$': 'ts-jest',
   },
   collectCoverageFrom: ['./src/**/*.ts'],
+  coverageReporters: [['github-actions', { silent: false }], 'summary'],
   testRegex: '(/test/.*test)\\.[jt]sx?$',
   testPathIgnorePatterns: ['/lib/', '/node_modules/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
